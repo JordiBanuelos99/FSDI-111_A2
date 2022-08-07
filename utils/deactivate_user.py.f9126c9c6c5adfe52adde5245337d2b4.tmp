a7f5682b140bf4ee9805201c873efd7235a37562
JordiBanuelos99 <banuelosmendez99@gmail.com>
2022-08-06 19:40:04 -0700

    Assignment 2

---
+++ utils/deactivate_user.py.f9126c9c6c5adfe52adde5245337d2b4.tmp
@@ -0,0 +1,18 @@
+import requests
+
+URL= "HTTP://127.0.0.1:5000/users"
+
+def deactivate_user(id):
+    url=URL+"/"+id
+    response = requests.delete(url)
+    if response.status_code == 204:
+        print (
+            "Done"
+        )
+    else:
+        print( "Something went wrong"
+        )
+
+if __name__ == "__main__":
+    id = input("Type in the user's ID to delete: ")
+    deactivate_user(id)
---
+++ utils/update_user.py
@@ -0,0 +1,27 @@
+import requests
+
+URL= "HTTP://127.0.0.1:5000/users"
+
+def update_user(id, first_name, last_name, hobbies):
+    user = {
+        "ID": id,
+        "first_name": first_name,
+        "last_name": last_name,
+        "hobbies": hobbies
+    }
+    url=URL+"/"+id
+    response = requests.put(url, json=user)
+    if response.status_code == 204:
+        print (
+            "Ok"
+        )
+    else:
+        print( "Something went wrong"
+        )
+
+if __name__ == "__main__":
+    id = input("Type in the user's ID: ")
+    fname = input("Type in the user's first name: ")
+    lname = input ("Type in the user's last name: ")
+    hobbies = input ("Type in the user's favorite hobbies: ")
+    update_user(id, fname, lname, hobbies)
